cdb0d566c82f34d50a95fbaa1c95f55bd621c88e
sesneha190529 <das05sneha@gmail.com>
2026-03-26 10:50:00 +0530

    Edited

--- server/src/sockets/webSocket.js
+++ server/src/sockets/webSocket.js
@@ -4,8 +4,13 @@ const Device = require("../models/device.model.js");
 const registerSocketHandlers = (io) => {
   io.use(socketDeviceValidation);
 
-  io.on("connection", (socket) => {
-    console.log("Device Connected: ", socket.device?._id);
+  io.on("connection", async (socket) => {
+    console.log("Device Connected:", socket.device?._id);
+
+    await Device.findByIdAndUpdate(socket.device._id, {
+      status: "online",
+    });
+
     socket.on("join-room", async ({ roomId, otherDeviceId }) => {
       try {
         socket.join(roomId);
@@ -13,25 +18,39 @@ const registerSocketHandlers = (io) => {
         const otherDevice = await Device.findById(otherDeviceId);
         if (!otherDevice) return;
 
-        socket.emit("device_info", {
+        socket.emit("device-info", {
           device: otherDevice.deviceName,
           user: otherDevice.usedBy,
         });
       } catch (error) {
-        console.log("Join room error: ", error.message);
+        console.log("Join room error:", error.message);
       }
     });
 
-    socket.on("send_location", ({ roomId, location }) => {
-      io.to(roomId).emit("recieve-location", {
-        sharedBy: socket.user._id,
-        device: socket.device.name,
-        location,
-      });
+    socket.on("send_location", async ({ roomId, location }) => {
+      try {
+        await Device.findByIdAndUpdate(socket.device._id, {
+          lastLocation: location,
+          lastUpdated: new Date(),
+        });
+
+        io.to(roomId).emit("receive-location", {
+          sharedBy: socket.user._id,
+          device: socket.device.deviceName,
+          location,
+        });
+      } catch (err) {
+        console.log("Location error:", err.message);
+      }
     });
 
-    socket.on("disconnect", () => {
-      console.log("Device is disconnected: ", socket.device?._id);
+    socket.on("disconnect", async () => {
+      await Device.findByIdAndUpdate(socket.device._id, {
+        status: "offline",
+        lastSeen: new Date(),
+      });
+
+      console.log("Device disconnected:", socket.device?._id);
     });
   });
 };
