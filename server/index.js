3dbcacfd055e625fef376727c1a283773dba1ee8
sesneha190529 <das05sneha@gmail.com>
2026-03-29 11:31:47 +0530

    Fixed

--- server/index.js
+++ server/index.js
@@ -1,27 +1,26 @@
-const { error } = require("console");
 const app = require("./src/app.js");
 const connectDB = require("./src/config/db.js");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
+const cors = require("cors");
 const passport = require("passport");
-
 
 const port = process.env.PORT || 3000;
 const server = createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5000",
-        methods: ["GET", "POST"],
-        credentials: true
-    }
-})
+  cors: {
+    origin: "http://localhost:5000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 connectDB()
-.then(() =>{
-     server.listen(port, () => {
+  .then(() => {
+    server.listen(port, () => {
       console.log(`Server is listening at http://localhost:${port}`);
     });
-})
-.catch((error) =>{
+  })
+  .catch((error) => {
     console.log("MongoDb connection failed", error.message);
-})
+  });
